bcce49b621a25b8dd8be05ab7c4eb5e705eaf54d
Tareq <tjarwan@atypon.com>
2018-08-08 11:05:49 +0300

    Creating the JWT

--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://TareqJarwan:T123456@ds215172.mlab.com:15172/dev_connector'
+    mongoURI: 'mongodb://TareqJarwan:T123456@ds215172.mlab.com:15172/dev_connector',
+    secretOrKey: 'secret'
 };
