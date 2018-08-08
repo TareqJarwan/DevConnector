9a74992d05a9596d8241bad430d3b2cc9ee84819
Tareq <tjarwan@atypon.com>
2018-08-08 14:37:40 +0300

    validation Handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,10 @@ const User = require('../../models/User');
 // Load Config
 const keys = require('../../config/key');
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
@@ -24,13 +28,22 @@ router.get('/test', (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({
         email: req.body.email
     }).then(user => {
         if (user) {
-            return res.status(400).json({
-                email: 'Email already exists'
-            });
+            errors.email = 'Email already exists';
+            return res.status(400).json(errors);
         } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200', //Size
@@ -63,6 +76,16 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateLoginInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -71,9 +94,8 @@ router.post('/login', (req, res) => {
         email
     }).then(user => {
         if (!user) {
-            return res.status(404).json({
-                email: 'User not found'
-            });
+            errors.email = 'User not found';
+            return res.status(404).json(errors);
         }
 
         // Check Password
@@ -98,14 +120,12 @@ router.post('/login', (req, res) => {
                         });
                     });
                 } else {
-                    return res.status(400).json({
-                        password: 'Password incoreect'
-                    });
+                    errors.password = 'Password incoreect';
+                    return res.status(400).json(errors);
                 }
             });
     });
 });
-
 
 // @route   GET api/users/current
 // @desc    Return Current User 
