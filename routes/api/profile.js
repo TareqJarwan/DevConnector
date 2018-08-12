fe323efe7815ca71b27ed7c4efb192d3892e41ab
Tareq <tjarwan@atypon.com>
2018-08-12 17:36:05 +0300

    Finishing the profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -264,8 +264,73 @@ router.post('/education', passport.authenticate('jwt', {
     });
 });
 
+// @route   Delete api/profile/experience/:exp_id
+// @desc    delete experience to profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {
+    seesion: false
+}), (req, res) => {
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        // Get romove index
+        const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
 
+        // Splice out of array    
+        profile.experience.splice(removeIndex, 1);
 
+        // Save
+        profile.save()
+            .then(profile => res.json(profile))
+            .catch(err => res.status(404).json(err));
+
+    });
+});
+
+// @route   Delete api/profile/education/:edu_id
+// @desc    delete education to profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', {
+    seesion: false
+}), (req, res) => {
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        // Get romove index
+        const removeIndex = profile.education
+            .map(item => item.id)
+            .indexOf(req.params.edu_id);
+
+        // Splice out of array    
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save()
+            .then(profile => res.json(profile))
+            .catch(err => res.status(404).json(err));
+
+    });
+});
+
+// @route   Delete api/profile
+// @desc    delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', {
+    seesion: false
+}), (req, res) => {
+    Profile.findOneAndRemove({
+        user: req.user.id
+    }).then(() => {
+        User.findOneAndRemove({
+                _id: req.user.id
+            })
+            .then(() => res.json({
+                success: true
+            }));
+    });
+});
 
 
 
