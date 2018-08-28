567e3712d5a2b78d7459adb855750f4e0e0168e4
Tareq <tjarwan@atypon.com>
2018-08-28 18:51:30 +0300

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -21,8 +22,8 @@ const db = require('./config/key').mongoURI;
 
 // Connect to MongoDB
 mongoose.connect(db, {
-        useNewUrlParser: true
-    }).then(() => console.log('MongoDB Connected'))
+    useNewUrlParser: true
+}).then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
 // Passport middleware
@@ -30,18 +31,28 @@ app.use(passport.initialize());
 
 // Passport Config
 require('./config/passport')(passport);
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
-  });
-  
-  passport.deserializeUser(function(user, done) {
+});
+
+passport.deserializeUser(function (user, done) {
     done(null, user);
-  });
+});
 
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
